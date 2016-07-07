b5a5bf58358951528c7ad6c3e761e5e0b79260c7
mario2904 <orbegoso2904@gmail.com>
2016-07-07 08:56:05 -0400

    Skelleton of working WebSocketServer done.

---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+//const express = require('express');
+const SocketServer = require('ws').Server;
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+
+const wss = new SocketServer({ port: PORT });
+console.log(`Listening on port: ${ PORT }`)
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  
+  // Handle request logic here
+  ws.on('message', (message) => {console.log('received:', message);});
+  ws.on('close', () => console.log('Client disconnected'));
+});
+
+// Broadcast the time every second
+setInterval(() => {
+  wss.clients.forEach((client) => {
+    client.send(new Date().toTimeString());
+  });
+}, 1000);
