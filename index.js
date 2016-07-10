ea497a03f6a42cc850418718e0a19855d8f8e477
mario2904 <orbegoso2904@gmail.com>
2016-07-10 17:26:39 -0400

    Started doing testing

--- index.js
+++ index.js
@@ -10,15 +10,27 @@ const PORT = process.env.PORT || 3000;
 const wss = new SocketServer({ port: PORT });
 console.log(`Listening on port: ${ PORT }`)
 
+/*
 wss.on('connection', (ws) => {
   console.log('Client connected');
-  
+
   // Handle request logic here
-  ws.on('message', (message) => {console.log('received:', message);});
+  ws.on('message', (message) => {console.log('received:', message)});
   ws.on('close', () => console.log('Client disconnected'));
+});
+*/
+
+wss.on('open', () => {
+  wss.send('Connection established :)')
+});
+
+wss.on('message', (message) => {
+  console.log('received:', message);
+  wss.send('Received the message: ' + message)
 });
 
 // Broadcast the time every second
+
 setInterval(() => {
   wss.clients.forEach((client) => {
     client.send(new Date().toTimeString());
